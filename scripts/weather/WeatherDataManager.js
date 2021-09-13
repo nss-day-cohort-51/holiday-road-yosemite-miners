68f7b61abfee6591ca2681fc04d32928aefd472e
tessema2021 <ninafuchere@gmail.com>
2021-09-13 11:12:33 -0500

    updates

--- scripts/weather/WeatherDataManager.js
+++ scripts/weather/WeatherDataManager.js
@@ -5,3 +5,5 @@ export const getWeather = () => {
     `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${settings.weatherKey}`
   ).then((response) => response.json());
 };
+
+//api.openweathermap.org/data/2.5/weather?zip={37167},{TN}&appid={API key}
