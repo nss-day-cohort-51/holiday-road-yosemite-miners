ee0fb32399867b5cf823367b4bfd719143045417
Matt F <trumpetman072392@gmail.com>
2021-09-16 09:32:17 -0500

    added code to fetch zip code for park

--- scripts/weather/Weather.js
+++ scripts/weather/Weather.js
@@ -1,6 +1,6 @@
 export const weatherForecastItem = (weatherObj) => {
     return `<article id="weather-info__item">
     <h5 class="day-name">original format: ${weatherObj.dt}</h5>
-    <p class="feels-like-temperature">Feels like ${weatherObj.temp}</p>
+    <p class="feels-like-temperature">Feels like ${weatherObj.main[0].temp}</p>
 </article>`
 }
